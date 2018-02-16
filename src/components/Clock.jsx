eee7fb124d6f13f0b12450721ba1fd1d40da79da
Gustavo Sobral <ghsobral@gmail.com>
2018-02-16 22:35:32 +0100

    Display clock on app header

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,35 +1,31 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
 
 import './Clock.css'
 
 class Clock extends Component {
-  renderMinute(timer) {
-    return this.formatTime(this.calculateMinute(timer))
-  }
+  calculateMinute = (timer) => Math.floor(timer / 60)
 
-  renderSecond(timer) {
-    return this.formatTime(this.calculateSecond(timer))
-  }
+  calculateSecond = (timer) => timer % 60
 
-  calculateMinute(timer) {
-    return Math.floor(timer / 60)
-  }
+  formatTime = (unit) => unit < 10 ? `0${unit}` : unit
 
-  calculateSecond(timer) {
-    return timer % 60
-  }
+  renderTimer = (timer) => `${this.formatTime(this.calculateMinute(timer))}:${this.formatTime(this.calculateSecond(timer))}`
 
-  formatTime(unit) {
-    return unit < 10 ? `0${unit}` : unit
+  renderHeader = (timer) => {
+    return (
+      <Helmet>
+        <title>Tomaat ({ this.renderTimer(timer) })</title>
+      </Helmet>
+    )
   }
 
   render() {
     return (
       <h1 className='clock'>
-        { this.renderMinute(this.props.timer) }
-        :
-        { this.renderSecond(this.props.timer) }
+        { this.renderTimer(this.props.timer) }
+        { this.renderHeader(this.props.timer) }
       </h1>
     )
   }
